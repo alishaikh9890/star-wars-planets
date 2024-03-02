bc7c6e004c9486a7b3d8592309f3bae756ab2af2
Alishan Shaikh <alishanshaikh9890@gmail.com>
2024-03-02 18:02:27 +0530

    lastone

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,6 +9,11 @@ function Navbar() {
     <img src="https://i.pinimg.com/originals/ed/a4/3a/eda43aca52541b9799b0b81b2fb47ddd.gif" alt=""/>
 
   <Link style={{margin:"auto", fontSize:"25px"}} to="/">STAR WARS PLANETS</Link>
+
+  <Link class="button" to="/">⬅ Home</Link>
+
+
+
  
     
     </div>
